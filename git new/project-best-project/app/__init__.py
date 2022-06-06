1fafc658ff51fd316f1533d199451c5de5b6b7f0
Lydia Farag <77176132+lydiafarag@users.noreply.github.com>
2022-06-05 21:19:53 -0400

    Changes to my branch
    - resolved issue #5 (add about me) and #6 ( add a pic) and #9 (menu bar) via updates in the index.jinja file and with an additional about.jinja file

---
+++ git new/project-best-project/app/__init__.py
@@ -0,0 +1,119 @@
+import os
+from flask import Flask, render_template, request
+from flask_googlemaps import GoogleMaps, Map
+from dotenv import load_dotenv
+from flask_nav import Nav
+from flask_nav.elements import *
+
+load_dotenv()
+app = Flask(__name__, )
+GoogleMaps(app, key=os.getenv("MAPS_API_KEY"))
+
+@app.route("/")
+def index():
+
+    # **Shape of Data**
+    #
+    # experiences = [{
+    #    "name": string,
+    #    "role": string,
+    #    "date": string
+    # }, ...]
+    #
+    # hobbies = [{
+    #    "name": string,
+    #    "img": string
+    # }, ...]
+    #
+    # education = [{
+    #    "name": string,
+    #    "location": string
+    # }, ...]
+
+    return render_template("main.jinja",
+                           title="Bobo the Baboon",
+                           url=os.getenv("URL"),
+                           experiences=[
+                               {
+                                   "name": "Meta",
+                                   "role": "Production Engineer",
+                                   "date": "XX 2021 - XX 2022"
+                               },
+                               {
+                                   "name": "Google",
+                                   "role": "Software Engineer",
+                                   "date": "XX 2020 - XX 2021"
+                               },
+                               {
+                                   "name": "Amazon",
+                                   "role": "Systems Development Engineer",
+                                   "date": "XX 2019 - XX 2020"
+                               },
+                           ],
+                           educations=[{
+                               "name": "Yale University",
+                               "location": "New Haven, CT"
+                           }, {
+                               "name": "Harvard University",
+                               "location": "Cambridge, MA"
+                           }, {
+                               "name": "Stanford University",
+                               "location": "Stanford, CA"
+                           }])
+
+
+@app.route("/hobbies/")
+def hobbies_and_map():
+    # creating a map in the view
+    title = "Bobo's Hobbies"
+
+    bobomap = Map(
+        identifier="bobomap",
+        lat=9.1304,
+        lng=41.2809,
+        markers=[
+            {
+                'icon': 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
+                'lat': 9.1304,
+                'lng': 41.2809,
+                'infobox': '<img src="https://cdn3.iconfinder.com/data/icons/142-mini-country-flags-16x16px/32/flag-denmark2x.png" width=32px height=32px/>',
+            },
+            {
+                'icon': 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png',
+                'lat': 23.6345,
+                'lng': -102.5528,
+                'infobox': '<img src="https://cdn3.iconfinder.com/data/icons/142-mini-country-flags-16x16px/32/flag-mexico2x.png" width=32px height=32px/>'
+            },  
+            {
+                'icon': 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+                'lat': 15.8700,
+                'lng': 100.9925,
+                'infobox': '<img src= "../static/img/bobo.jpg" width=32px height=32px/>'
+            },
+            {
+                'icon': 'http://maps.google.com/mapfiles/ms/icons/red-dot.png',
+                'lat': 64.9631,
+                'lng': -19.0208,
+                'infobox': '<img src= "https://cdn3.iconfinder.com/data/icons/142-mini-country-flags-16x16px/32/flag-iceland2x.png" width=32px height=32px/>'
+            }    
+        ],
+        style="height:600px;width:1200px;margin:auto",
+        zoom=2)
+
+    hobbies = [
+        {
+            "name": "Gaming",
+            "img": "./img/game.jpg"
+        },
+        {
+            "name": "Working Out",
+            "img": "./img/gym.jpg"
+        },
+    ]
+
+    return render_template('hobbies_and_map.jinja',
+                           title=title,
+                           hobbies=hobbies,
+                           trdmap=bobomap,
+                           name= "Bobo", 
+                           url=os.getenv("URL"))

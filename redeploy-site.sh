f0d6a232c1d72376cdee75940a2d8a3d1627bae5
Shawn Zhang <shawnzhang12@gmail.com>
2022-06-24 21:41:17 +0000

    using system services for redeployment instead of tmux

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-#Store and run this file in the same directory where this git repository is stored
-
-tmux kill-server || echo "No tmux server to kill"
-
 cd ~/flask-personal-portfolio
 
 git fetch && git reset origin/main --hard
@@ -11,5 +7,5 @@ git fetch && git reset origin/main --hard
 source python3-virtualenv/bin/activate
 pip3 install -r requirements.txt
 
-tmux new-session -d -s myportfolio 'source python3-virtualenv/bin/activate && \
-export FLASK_ENV=development && flask run --host=0.0.0.0'
+systemctl daemon-reload
+systemctl restart myportfolio
